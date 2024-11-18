7fa9bb1b5574ce4c50fef984926c454be13fcd95
Your Name <your_email@example.com>
2024-11-18 04:11:08 +0300

    "Removed react-native-community-geolocation dependency and related code"

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,72 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, PermissionsAndroid, Platform} from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 import YaMap from 'react-native-yamap';
 
 const Map = () => {
-  const [userLocation, setUserLocation] = useState(null);
-
-  useEffect(() => {
-    const requestLocationPermission = async () => {
-      if (Platform.OS === 'android') {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: 'Разрешение на доступ к местоположению',
-            message: 'Приложению требуется доступ к вашему местоположению для отображения карты.',
-            buttonNeutral: 'Спросить позже',
-            buttonNegative: 'Отмена',
-            buttonPositive: 'ОК',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          getUserLocation();
-        } else {
-          console.log('Доступ к местоположению отклонен');
-        }
-      } else {
-        getUserLocation();
-      }
-    };
-
-    const getUserLocation = () => {
-      Geolocation.getCurrentPosition(
-        position => {
-          const {latitude, longitude} = position.coords;
-          setUserLocation({latitude, longitude});
-        },
-        error => console.log(error),
-        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-      );
-    };
-
-    requestLocationPermission();
-  }, []);
-
   return (
     <View style={styles.main}>
-      {userLocation && (
         <YaMap
           style={styles.map}
           showUserPosition={true}
-          initialRegion={{
-            lat: userLocation.latitude,
-            lon: userLocation.longitude,
-            zoom: 15,
-          }}
+					
         />
-      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-  },
+	main: {
+		flex: 1,
+	},
   map: {
-    width: '100%',
-    height: '100%',
+		width: '100%',
+  	height: '100%',
   },
 });
 
